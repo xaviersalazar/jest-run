cd6c277689f3942290678878c41be06a101084b8
Xavier Salazar <salazar.xavier26@gmail.com>
2020-01-25 13:50:49 -0600

    add run commands/lenses for describe and it tests

--- src/codeLensProvider.ts
+++ src/codeLensProvider.ts
@@ -1,39 +1,69 @@
 import * as vscode from "vscode";
+import { parse } from "jest-editor-support";
 
 export class CodeLensProvider implements vscode.CodeLensProvider {
   private codeLenses: vscode.CodeLens[] = [];
+
+  private createRunCommand = (args: {
+    file: string;
+    name: string;
+    type: string;
+  }): vscode.Command => {
+    return {
+      command: "jestRun.run",
+      title: args.type === "describe" ? "Run block" : "Run",
+      arguments: [`npm test -- -u -t=\"${args.name}\"`],
+      tooltip: "Run"
+    };
+  };
+
+  private createLens = (
+    line: number,
+    column: number,
+    args: {
+      file: string;
+      name: string;
+      type: string;
+    }
+  ): vscode.CodeLens => {
+    const lens = new vscode.Range(line - 1, column - 1, line - 1, column - 1);
+    const runCodeLens = new vscode.CodeLens(lens, this.createRunCommand(args));
+
+    return runCodeLens;
+  };
 
   public provideCodeLenses(
     document: vscode.TextDocument
   ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
     this.codeLenses = [];
-    const regex = new RegExp(/(it\()/gm);
-    const text = document.getText();
-    let matches;
 
-    while ((matches = regex.exec(text)) !== null) {
-      const line = document.lineAt(document.positionAt(matches.index).line);
-      const indexOf = line.text.indexOf(matches[0]);
-      const position = new vscode.Position(line.lineNumber, indexOf);
-      const range = document.getWordRangeAtPosition(
-        position,
-        new RegExp(regex)
-      );
-      if (range) {
-        this.codeLenses.push(new vscode.CodeLens(range));
-      }
+    const path = document.uri.fsPath;
+    const parsedPath = parse(path);
+
+    if (parsedPath.describeBlocks.length > 0) {
+      parsedPath.describeBlocks.forEach(db => {
+        const lens = this.createLens(db.start.line, db.start.column, {
+          file: db.file,
+          name: db.name,
+          type: db.type
+        });
+
+        this.codeLenses.push(lens);
+      });
+    }
+
+    if (parsedPath.itBlocks.length > 0) {
+      parsedPath.itBlocks.forEach(it => {
+        const lens = this.createLens(it.start.line, it.start.column, {
+          file: it.file,
+          name: it.name,
+          type: it.type
+        });
+
+        this.codeLenses.push(lens);
+      });
     }
 
     return this.codeLenses;
-  }
-
-  public resolveCodeLens(codeLens: vscode.CodeLens) {
-    codeLens.command = {
-      title: "Run",
-      tooltip: "Jest Run",
-      command: "jestRun.run",
-      arguments: ["jest -t", true]
-    };
-    return codeLens;
   }
 }
