c8599c6e631d92c88f2074fbfa40c54c2b224fe0
Xavier Salazar <salazar.xavier26@gmail.com>
2020-01-25 18:12:09 -0600

    add test file pattern matchers for codelens

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 import * as vscode from "vscode";
 import { CodeLensProvider } from "./codeLensProvider";
+import { TEST_FILE_PATTERNS } from "./utils";
 
 export function activate(context: vscode.ExtensionContext) {
-  const codeLensProvider = new CodeLensProvider();
-
-  vscode.languages.registerCodeLensProvider("*", codeLensProvider);
+  vscode.languages.registerCodeLensProvider(
+    TEST_FILE_PATTERNS.map(tfp => ({
+      pattern: tfp,
+      scheme: "file"
+    })),
+    new CodeLensProvider()
+  );
 
   vscode.commands.registerCommand("jestRun.run", args => {
     const terminal = vscode.window.createTerminal("Jest Run");
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export const TEST_FILE_PATTERNS = [
+  "**/*.{test,spec}.{js,jsx,ts,tsx}",
+  "**/__tests__/*.{js,jsx,ts,tsx}"
+];
