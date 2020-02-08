73a6b8d04e32f9a7d42e24b14f88277e797e2e9e
Xavier Salazar <salazar.xavier26@gmail.com>
2020-02-07 20:08:39 -0600

    updating package

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,13 @@ import * as vscode from "vscode";
 import { CodeLensProvider } from "./codeLensProvider";
 import { TEST_FILE_PATTERNS } from "./utils";
 
+const createTerminal = (args: string) => {
+  const terminal = vscode.window.createTerminal("Jest Run");
+
+  terminal.show();
+  terminal.sendText(args);
+};
+
 export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerCodeLensProvider(
     TEST_FILE_PATTERNS.map(tfp => ({
@@ -12,10 +19,7 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   vscode.commands.registerCommand("jestRun.run", args => {
-    const terminal = vscode.window.createTerminal("Jest Run");
-
-    terminal.show();
-    terminal.sendText(args);
+    createTerminal(args);
   });
 }
 
